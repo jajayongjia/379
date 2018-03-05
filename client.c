2ddfecf619790de24dc81b6d7f239a60eca43856
Yongjia Huang <yongjia@cs.ualberta.ca>
2018-03-05 13:41:59 -0700

    able to handle one client

--- client.c
+++ client.c
@@ -12,32 +12,39 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <ncurses.h>
-
+#include <time.h>
 
 #define	 MY_PORT  7777
 
-
+#define HEIGHT 12
+#define WIDTH 12
+#define START_Y 10
+#define START_X 10
 /* ---------------------------------------------------------------------
    This is a sample client program for the number server. The client and
    the server need to run on the same machine.
    --------------------------------------------------------------------- */
+
 struct playerPosition{
     int *new_sock;
 	int id;
 	int x;
 	int y;
+	char direction[2];
+	char move;
 };
 
-void draw(int y,int x,struct playerPosition *player,WINDOW* window,int height,int width);
+void changePosition(struct playerPosition *player,WINDOW* win);
 
-void drawScreen(WINDOW* win,int height,int width);
+void drawScreen(WINDOW* win,struct playerPosition *player);
+
 
 int main()
 {	
-	
+
 	int	s, number;
     struct playerPosition *player;
 	struct	sockaddr_in	server;
@@ -71,29 +78,33 @@ int main()
 		exit (1);
 	}
 
+
 	initscr();
 	cbreak();
-	int height,width,start_y,start_x;
-	height=width=12;
-	start_y = start_x =10;
-	WINDOW* win = newwin(height,width,start_y,start_x);
-	refresh();
 	
-	drawScreen(win,height,width);
+	WINDOW* win = newwin(HEIGHT,WIDTH,START_Y,START_X);
+	refresh();
+
+
+
 	while(1){
     	player = malloc(sizeof(struct playerPosition));
 
 	recv(s,player,sizeof(struct playerPosition),0);
+	// full contents with x, y , direction
+	player->move = '?';
+	drawScreen(win,player);
+        unsigned int reTime = time(0) +1;
+        while (time(0)<reTime){
 	
-	wclear(win);
+	changePosition(player,win);
+	}
 	
-	drawScreen(win,height,width);
-	draw(player->y,player->x,player,win,height,width);
-
+		
+        // player only contains new move direction
 	send(s,player,sizeof(struct playerPosition),0);
 
-   // printf("\nplayer id: %d\nx: %d \ny: %d\n",player->id,player->x,player->y);
-	
+   
 	
 
     free(player);
@@ -101,7 +112,8 @@ int main()
 	close (s);
 }
 
-void drawScreen(WINDOW* win,int height,int width){
+void drawScreen(WINDOW* win,struct playerPosition *player){
+	wclear(win);	
 	char lr = '|';
 	char tb = '-';
 	box(win,(int)lr,(int)tb);
@@ -109,87 +121,49 @@ void drawScreen(WINDOW* win,int height,int width){
 	wrefresh(win);
 
 	mvwprintw(win,0,0,"+");
-	mvwprintw(win,0,height-1,"+");
-	mvwprintw(win,width-1,0,"+");
-	mvwprintw(win,width-1,height-1,"+");
+	mvwprintw(win,0,HEIGHT-1,"+");
+	mvwprintw(win,WIDTH-1,0,"+");
+	mvwprintw(win,WIDTH-1,HEIGHT-1,"+");
+        strcat(player->direction,"\0");
+	mvwprintw(win,player->y,player->x,player->direction);
 	wrefresh(win);
 }
 
-// void draw(int y,int x,struct playerPosition *player,WINDOW* win,int height, int width){
-// 	char *direction,*p;
-// 	direction = malloc(sizeof(char)*5);
-	
-// 	strcpy(direction,"?");
 
-// 	mvwprintw(win,y,x,direction);
-// 	wrefresh(win);
-	
-// 	keypad(win,true);
-// 	int c = wgetch(win);
-// 	switch(c){
-// 		case KEY_UP:
-// 			if(player->y>1)
-// 			player->y-=1;
-// 			*p = "^";
-// 			break;
-// 		case KEY_DOWN:
-// 			if(player->y<height-2)
-// 			player->y+=1;
-// 			*p = "v";
-// 			break;
-// 		case KEY_LEFT:
-// 			if(player->x>1)
-// 			player->x-=1;
-// 			*p="<";
-// 			break;
-// 		case KEY_RIGHT:
-// 			if(player->x<width-2)
-// 			player->x+=1;
-// 			*p = ">";
-// 			break;	
-// 	}
-
-// 	wrefresh(win);
-// 	mvwprintw(win,y,x,direction);
-// 	wrefresh(win);
-// 	free(direction);
-// }
-void draw(int y,int x,struct playerPosition *player,WINDOW* win,int height, int width){
-	char *direction;
-	direction = malloc(sizeof(char)*5);
-	
-	strcpy(direction,"?");
-
-	mvwprintw(win,y,x,direction);
-	wrefresh(win);
-	
+void changePosition(struct playerPosition *player,WINDOW* win){
 	keypad(win,true);
+
 	int c = wgetch(win);
 	switch(c){
 		case KEY_UP:
-			if(player->y>1)
-			player->y-=1;
-				strcpy(direction,"^");
+			
+	//		if(player->y>1)
+	//		player->y-=1;
+			player->move='^';
+		
 			break;
 		case KEY_DOWN:
-			if(player->y<height-2)
-			player->y+=1;
-				strcpy(direction,"v");
+	//		if(player->y<HEIGHT-2)
+	//		player->y+=1;
+			player->move='v';
 			break;
 		case KEY_LEFT:
-			if(player->x>1)
-			player->x-=1;
-				strcpy(direction,"<");
+	//		if(player->x>1)
+	//		player->x-=1;
+			player->move='<';
 			break;
 		case KEY_RIGHT:
-			if(player->x<width-2)
-			player->x+=1;
-				strcpy(direction,">");
+	//		if(player->x<WIDTH-2)
+	//		player->x+=1;
+			player->move='>';
 			break;	
 	}
+	
+	
 
-	wrefresh(win);
-	mvwprintw(win,y,x,direction);
-	wrefresh(win);
-	free(direction);
+	
 }
+
+
+
+
---
+++ server.c
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2017 Yongjia Huang, Yuhan Ye,CMPUT379, University of Alberta, All Rights Reserved.
+ * You may use distribut, or modify this code under terms and conditions of the ode of Student Behavior at University of Alberta
+ * You may find a copy of the license in this project. Otherwise please contact jajayongjia@gmail.com
+ */
+
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include<pthread.h>
+#define HEIGHT 12
+#define WIDTH 12
+#define START_Y 10
+#define START_X 10
+#define	MY_PORT	7777
+
+struct playerPosition{
+    int *new_sock;
+	int id;
+	int x;
+	int y;
+	char direction[2];
+	char move;
+};
+void *connection_handler(void *player1)
+{
+    //Get the socket descriptor
+    struct playerPosition *currentPlayer;
+    currentPlayer = malloc(sizeof(struct playerPosition));
+    currentPlayer = (struct playerPosition *) player1;
+    int sock = *(currentPlayer->new_sock);
+
+    send(sock,currentPlayer,sizeof(struct playerPosition),0);
+    	// Now we receive from the client, we specify that we would like 11 bytes
+	while (recv(sock,currentPlayer,sizeof(struct playerPosition),0)){
+ 
+        // the switch statment below should check all constraint of all the clients,
+        // and do the update 
+ 
+	switch(currentPlayer->move){
+		case '^':
+
+			if(currentPlayer->y>1){
+				currentPlayer->y-=1;
+				strcpy(currentPlayer->direction,"^"); 
+			}
+			break;
+		case 'v':
+
+			if(currentPlayer->y<HEIGHT-2){
+				currentPlayer->y+=1;
+				strcpy(currentPlayer->direction,"v"); 
+			}
+			break;
+		case '<':
+
+			if(currentPlayer->x>1){
+				currentPlayer->x-=1;
+				strcpy(currentPlayer->direction,"<"); 
+			}
+			break;
+		case '>':
+
+			if(currentPlayer->x<WIDTH-2){
+				currentPlayer->x+=1;
+				strcpy(currentPlayer->direction,">"); 
+			}
+			break;
+	}
+
+        send(sock,currentPlayer,sizeof(struct playerPosition),0);
+	}
+    free(currentPlayer);
+	
+    pthread_exit("");
+}
+int main(int argc, char * argv[])
+{
+	int	sock, snew, fromlength, number, outnum;
+
+	struct	sockaddr_in	master, from;
+	pthread_t thread_id;
+	// char c[11];
+
+
+
+	int i = 0;
+
+	sock = socket (AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) {
+		perror ("Server: cannot open master socket");
+		exit (1);
+	}
+
+	master.sin_family = AF_INET;
+	master.sin_addr.s_addr = inet_addr("127.0.0.1");
+	master.sin_port = htons (MY_PORT);
+
+	if (bind (sock, (struct sockaddr*) &master, sizeof (master))) {
+		perror ("Server: cannot bind master socket");
+		exit (1);
+	}
+
+	listen (sock, 5);
+	fromlength = sizeof (from);
+    struct playerPosition *player1;
+    player1 = malloc(sizeof(struct playerPosition));
+     player1->new_sock = malloc(1);
+    player1->x = 2;
+    player1->y = 2;
+    strcpy(player1->direction,">"); 
+    player1->move = '?';
+
+    /*******************/
+    while (snew = accept (sock, (struct sockaddr*) & from, & fromlength))
+    {
+
+        puts("Connection accepted");
+
+        pthread_t thread1;
+        *(player1->new_sock) = snew;
+
+        if( pthread_create( &thread1 , NULL ,  connection_handler , (void*) player1) < 0)
+        {
+            perror("could not create thread");
+            return 1;
+        }
+
+        //Now join the thread , so that we dont terminate before the thread
+        //pthread_join( thread1 , NULL);
+        puts("Handler assigned");
+    }
+
+    if (snew < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+    /*******************/
+
+
+
+
+
+	close (snew);
+}
+
